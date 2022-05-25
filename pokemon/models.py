9109c17aeb1ddac71899b404d16ef66b4bcbea63
Seun Dawodu <seun.dawodu1@gmail.com>
2022-05-25 14:39:07 +0100

    Add pokemon app to project

---
+++ pokemon/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Pokemon(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=256)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
