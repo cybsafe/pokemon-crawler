bd8f3f7f86392f7deb515c00e48a6ae761039190
Seun Dawodu <seun.dawodu1@gmail.com>
2022-05-25 16:06:01 +0100

    Testing notes

--- pokemon/tests.py
+++ pokemon/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestInitialDataMigration(TestCase):
+    # Test migrations work correctly.
+    # As migrations run before the test suite, some mocking willl be required
+    # or perhaps the use of https://github.com/wemake-services/django-test-migrations
+    # or similar
+    pass
+
+
+class TestPokemonAPI(TestCase):
+
+    # Tests to ensure we're handling the data from the Pokemon API correctly
+    # Mocking will be required here
+    # PACT testing may be a good idea if it happens to be available
+    pass
+
+
+class TestOutputAPI(TestCase):
+    # Tests to make sure we're presenting the right thing.
+    # There's no real logic being added, so we'd essentially be re-testing
+    # the rest_framework. Probably doesn't require a huge amount of unit testing
+    # provided we're happy with the framework.
+    pass
